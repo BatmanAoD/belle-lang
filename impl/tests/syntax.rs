f16924ba5df059d7ba6345c093b918869d6fc5bb
Kyle Strand <kyle.strand@pieinsurance.com>
2020-06-21 22:17:12 -0600

    Use built-in error formatting

--- impl/tests/syntax.rs
+++ impl/tests/syntax.rs
@@ -3,9 +3,10 @@ pub use pest::Parser;
 use belle::*;   // XXX TEMP
 
 fn parse_str(prog: &str) {
-    let result = BelleParser::parse(Rule::Program, prog);
-    println!("{:#?}", result);
-    assert!(result.is_ok());
+    if let Err(e) = BelleParser::parse(Rule::Program, prog) {
+        eprintln!("{}", e);
+        assert!(false, "Failed to parse");
+    }
 }
 
 #[test]
